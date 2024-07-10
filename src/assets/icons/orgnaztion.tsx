c5698c7eb96ba43d4a04c50496e68ee43ae389e2
H-7117 <hamod2131.a@gmail.com>
2024-07-10 17:04:03 +0300

    done

---
+++ src/assets/icons/orgnaztion.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+
+export default function Orgnaztion() {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M0.400024 16.7988H6.00004V22.3988H0.400024V16.7988Z"
+        fill="white"
+        fill-opacity="0.5"
+      />
+      <path
+        d="M6.39998 22.7984H0V16.3984H6.39998V22.7984ZM0.800016 21.9984H5.60002V17.1984H0.800016V21.9984Z"
+        fill="black"
+        fill-opacity="0.54"
+      />
+      <path
+        d="M18 16.7988H23.6V22.3988H18V16.7988Z"
+        fill="white"
+        fill-opacity="0.5"
+      />
+      <path
+        d="M24 22.7984H17.6V16.3984H24V22.7984ZM18.3999 21.9984H23.1999V17.1984H18.3999V21.9984Z"
+        fill="black"
+        fill-opacity="0.54"
+      />
+      <path
+        d="M9.20007 16.7988H14.8V22.3988H9.20007V16.7988Z"
+        fill="white"
+        fill-opacity="0.5"
+      />
+      <path
+        d="M15.2 22.7984H8.79993V16.3984H15.1999V22.7984H15.2ZM9.59994 21.9984H14.3999V17.1984H9.59994V21.9984Z"
+        fill="black"
+        fill-opacity="0.54"
+      />
+      <path
+        d="M9.20007 1.59961H14.8V7.19963H9.20007V1.59961Z"
+        fill="white"
+        fill-opacity="0.5"
+      />
+      <path
+        d="M15.2 7.5992H8.79993V1.19922H15.1999V7.5992H15.2ZM9.59994 6.79923H14.3999V1.99923H9.59994V6.79923Z"
+        fill="black"
+        fill-opacity="0.54"
+      />
+      <path
+        d="M21.2 16.7992H20.3999V12.3992H12.3999V16.7992H11.6V12.3992H3.59994V16.7992H2.79993V11.5992H11.6V7.19922H12.3999V11.5992H21.2V16.7992Z"
+        fill="black"
+        fill-opacity="0.54"
+      />
+    </svg>
+  );
+}
