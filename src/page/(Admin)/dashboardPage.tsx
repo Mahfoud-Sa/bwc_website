3cd7e36278275cecbc3d49455b78643d5af7d844
H-7117 <hamod2131.a@gmail.com>
2024-07-21 13:01:19 +0300

    done

--- src/page/(Admin)/dashboardPage.tsx
+++ src/page/(Admin)/dashboardPage.tsx
@@ -1,33 +1,54 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import toast, { Toaster } from "react-hot-toast";
 import NavBar from "../../components/(Admin)/navBar";
 import SideNab from "../../components/(Admin)/sideNab";
 import { useNavigate } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 
 export default function DashboardPage() {
   const navigate = useNavigate();
+  const { t, i18n } = useTranslation();
+  const dir = i18n.dir();
+
   const isLoggedIn = localStorage.getItem("accessToken");
   useEffect(() => {
     if (isLoggedIn) {
-      toast.success("You are logged in successfully!"); 
+      toast.success("You are logged in successfully!");
     }
     if (!isLoggedIn) {
-      navigate('/NoAccess');
+      navigate("/NoAccess");
     }
   }, []);
+
   return (
-    
-    <div className="h-[100vh] w-full box-border">
-      <Toaster />
-      <div className="h-[10vh] w-full flex">
-        <NavBar />
-      </div>
-      <div className="w-full h-[90vh] flex">
-        <div className=" w-[85%] h-full bg-slate-700"></div>
-        <aside className=" w-[15%] h-full ">
-          <SideNab />
-        </aside>
-      </div>
-    </div>
+    <>
+      {dir === "ltr" ? (
+        <div className="h-[100vh] w-full box-border">
+          <Toaster />
+          <div className="h-[10vh] w-full flex">
+            <NavBar />
+          </div>
+          <div className="w-full h-[90vh] flex">
+            <aside className=" w-[15%] h-full ">
+              <SideNab />
+            </aside>
+            <div className=" w-[85%] h-full bg-slate-700"></div>
+          </div>
+        </div>
+      ) : (
+        <div className="h-[100vh] w-full box-border">
+          <Toaster />
+          <div className="h-[10vh] w-full flex">
+            <NavBar />
+          </div>
+          <div className="w-full h-[90vh] flex">
+            <aside className=" w-[15%] h-full ">
+              <SideNab />
+            </aside>
+            <div className=" w-[85%] h-full bg-slate-700"></div>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
