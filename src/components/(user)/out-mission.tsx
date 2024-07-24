11291f30631629634641a4a71558cdd2ca67722f
H-7117 <hamod2131.a@gmail.com>
2024-07-24 10:26:40 +0300

    not complete yet

--- src/components/(user)/out-mission.tsx
+++ src/components/(user)/out-mission.tsx
@@ -1,62 +1,73 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 
 export default function OutMission() {
+  const { t, i18n } = useTranslation();
+  const dir = i18n.dir();
   return (
     <div>
       <div className="px-5">
-        <h1 className="text-5xl">مهمتنا</h1>
-        <p className="mt-8 text-xl text-[#5B5B5B]">
-          أن تكون شركة عالم الاعمال للاستثمار والدراسات المحدودة الشركة المتخصصة
-          والرائدة في إدارة وتشغيل المشاريع الاستثمارية وتقديم الدراسات
-          والاستشارات الإدارية دات الجودة العالية في حضرموت .
-        </p>
+        <h1 className="text-5xl">{t("our_mission1")}</h1>
+        <p className="mt-8 text-xl text-[#5B5B5B]">{t("our_missionDesc")}</p>
       </div>
       {/*  */}
       <div className="px-5 mt-20">
-        <h1 className="text-5xl">رسالتنا</h1>
+        <h1 className="text-5xl">{t("our_goals1")}</h1>
         <p className="mt-8 text-xl text-[#5B5B5B] leading-10">
-          شركة عالم الاعمال هي شركة متخصصة تقدم حدمات عالية الجودة في مجال
-          الاستثمار وإدارة المشاريع وريادة الاعمال من خلال الدراسات والاستثمارات
-          الادارية و المالية والتسويقية وحلول تكنولوجيا المعلومات عالية القيمة
-          لتمكين المستثمرين وأصحاب المصلحة من الحصول على أفضل النتائج والمنافع
-          الاقتصادية
+          {t("our_goalsDesc")}
         </p>
       </div>
       {/*  */}
       <div className="px-5 mt-20">
-        <h1 className="text-5xl">أهدافنا</h1>
+        <h1 className="text-5xl">{t("our_message1")}</h1>
         <div className="mt-10">
           <div className="flex items-center">
-            <span className=" block w-7 h-7 border-2 border-[#333333] bg-gradient-to-r from-[#997740] to-[#CCA972] ml-2"></span>
-            <p>
-              تمكين ومساعده المستثمرين وأصحاب الاعمال لإدارة وتشغيل مشاريعهم
-              الاستثمارية في حضرموت وفق أحدث النظم والاساليب الإدارية الحديثة
-              والمبتكرة.
+            <span
+              className={
+                dir === "ltr"
+                  ? " block w-7 h-7 border-2 border-[#333333] bg-gradient-to-r from-[#997740] to-[#CCA972] mr-2"
+                  : " block w-7 h-7 border-2 border-[#333333] bg-gradient-to-r from-[#997740] to-[#CCA972] ml-2"
+              }
+            ></span>
+            <p className="text-[#5B5B5B]">{t("our_messageDesc1")}</p>
+          </div>
+          {/*  */}
+          <div className="flex items-center mt-10">
+            <span
+              className={
+                dir === "ltr"
+                  ? " block w-7 h-7 border-2 border-[#333333] bg-gradient-to-r from-[#997740] to-[#CCA972] mr-2"
+                  : " block w-7 h-7 border-2 border-[#333333] bg-gradient-to-r from-[#997740] to-[#CCA972] ml-2"
+              }
+            ></span>
+            <p className="text-[#5B5B5B]">
+            {t("our_messageDesc2")}
             </p>
           </div>
           {/*  */}
           <div className="flex items-center mt-10">
-            <span className=" block w-7 h-7 border-2 border-[#333333] bg-gradient-to-r from-[#997740] to-[#CCA972] ml-2"></span>
-            <p>
-              الاهتمام والرعاية لرواد الأعمال وأصحاب المشاريع الناشئة من خلال
-              عمل الدراسات والاستشارات الفاعلة لتمكينهم من المنافسة والاستمرارية
-              في السوق.
+            <span
+              className={
+                dir === "ltr"
+                  ? " block w-7 h-7 border-2 border-[#333333] bg-gradient-to-r from-[#997740] to-[#CCA972] mr-2"
+                  : " block w-7 h-7 border-2 border-[#333333] bg-gradient-to-r from-[#997740] to-[#CCA972] ml-2"
+              }
+            ></span>
+            <p className="text-[#5B5B5B]">
+            {t("our_messageDesc3")}
             </p>
           </div>
           {/*  */}
           <div className="flex items-center mt-10">
-            <span className=" block w-7 h-7 border-2 border-[#333333] bg-gradient-to-r from-[#997740] to-[#CCA972] ml-2"></span>
-            <p>
-              تعزيز أداء الشركات والمؤسسات إداريا وماليا من خلال تطبيق أنظمة
-              إدارية نوعية ومبتكرة .
-            </p>
-          </div>
-          {/*  */}
-          <div className="flex items-center mt-10">
-            <span className=" block w-7 h-7 border-2 border-[#333333] bg-gradient-to-r from-[#997740] to-[#CCA972] ml-2"></span>
-            <p>
-              مساعدة الشركات والمؤوسسات الخاصة والعامة لاستخدام تطبيقات
-              تكنولوجيا المعلومات .
+            <span
+              className={
+                dir === "ltr"
+                  ? " block w-7 h-7 border-2 border-[#333333] bg-gradient-to-r from-[#997740] to-[#CCA972] mr-2"
+                  : " block w-7 h-7 border-2 border-[#333333] bg-gradient-to-r from-[#997740] to-[#CCA972] ml-2"
+              }
+            ></span>
+            <p className="text-[#5B5B5B]">
+            {t("our_messageDesc4")}
             </p>
           </div>
         </div>
--- src/page/(User)/aboutUs.tsx
+++ src/page/(User)/aboutUs.tsx
@@ -5,13 +5,13 @@ import OutMission from "../../components/(user)/out-mission";
 export default function AboutUs() {
   return (
     <div>
-      <div className="w-full lg:h-[8.45vh] sm:h-[11vh]">
+      <div className="w-full lg:h-[8vh] md:h-[8vh]  sm:h-[11vh]">
         <Navbar />
       </div>
-      <div className="  lg:h-[80vh] grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-4 px-4 relative sm:h-[140vh] lg:mt-8 sm:mt-0">
+      <div className="w-full h-[90vh] md:h-[150vh] md:mt-11 sm:mt-8">
         <AboutUsHeader />
       </div>
-      <div className="lg:w-[98%] lg:min-h-[80vh] m-auto mb-16">
+      <div className="lg:w-[98%] lg:min-h-[80vh] m-auto mb-16  lg:mt-16 sm:mt-40">
         <OutMission/>
       </div>
     </div>
