83f5a72da42be74508aab2ae5ee5e7c30bebb2df
H-7117 <hamod2131.a@gmail.com>
2024-07-24 13:34:22 +0300

    completed

--- src/components/(user)/aboutUs-header.tsx
+++ src/components/(user)/aboutUs-header.tsx
@@ -36,25 +36,162 @@ export default function AboutUsHeader() {
     <>
       <div>
         {isMobileScreen && (
-          <div className="w-full h-[90vh] grid grid-cols-1 gap-2 px-2">
-            <div className="h-full w-full bg-slate-800"></div>
-            <div className="h-full w-full bg-slate-800"></div>
-          </div>
+          <>
+            {dir === "ltr" ? (
+              <div className="w-full h-[100vh] grid grid-cols-1 gap-2 px-4">
+              <div className="h-[70vh] w-full relative flex justify-center items-center ">
+                <div className="absolute sm:w-[25%] sm:h-[55%] bg-[#E4C189] -z-[10] sm:top-[20px]  sm:left-[40px]"></div>
+                <div className="w-[80%] h-[80%] bg-white relative">
+                  <img
+                    src={Image1}
+                    className="object-cover w-full h-[80%]"
+                    alt=""
+                  />
+                </div>
+                <div className="absolute flex justify-center  md:rounded-md items-center w-[55%] h-[35%] bg-[#fff] z-[10]  bottom-[80px] right-0">
+                  <img
+                    src={Image1}
+                    className=" w-[92%] h-[88%] round"
+                    alt=""
+                  />
+                </div>
+              </div>
+              <div className="h-full w-full">
+                <div className=" pl-4 ">
+                  <div className="flex items-start justify-start py-16">
+                    <div className="w-3 h-10 rounded-md bg-[#CCA972] mr-2 bg-gradient-to-r from-[#A27942] "></div>
+                    <h1 className="text-3xl font-extrabold">
+                      {t("homePage2")}
+                    </h1>
+                  </div>
+                  <div className="text-start">
+                    <p className="text-justify  text-[#5B5B5B] leading-7">
+                      شركه عالم الاعمال هي شركه وطنية ذات مسؤولية محدودة.أسسها
+                      الدكتور ربيع بن علي العوبثاني في مدينة المكلا و حضرموت
+                      في مارس 2021م تحمل سجل تجاري رقم(21/2831).
+                    </p>
+                    <p className="text-justify  mt-8 text-[#5B5B5B] leading-8">
+                      تعمل على إدارة وتشغيل المشاريع الاستثمارية وتقديم
+                      الدراسات الاقتصادية والاستشارات الادارية والمالية
+                      والتسويقية وحلول تكنولوجيا المعلومات وتقدم خدماتها من
+                      خلال مجموعة من الخبراء والاستشاريين المتخصصين الذين
+                      يعملون في الشركة ومجموعة اخرى من الخبراء والاستشاريين
+                      المتعاقدين.
+                    </p>
+                  </div>
+                  <div className="grid grid-cols-3 gap-2 mt-6">
+                    <div className=" flex justify-start items-center">
+                      <span className=" w-[40%] h-[100%] rounded-full bg-gradient-to-r from-[#997740] to-[#CCA972] flex justify-center items-center mr-2">
+                        <img src={partnership} className="w-[100%] h-[80%]" alt="" />
+                      </span>
+                      <p className="text-xl">{t("our_mission")}</p>
+                    </div>
+                    <div className=" flex justify-start items-center">
+                      <span className=" w-[40%] h-[100%] rounded-full bg-gradient-to-r from-[#997740] to-[#CCA972] flex justify-center items-center mr-2 ml-2">
+                        <img src={golas} className="w-[80%] h-[80%]" alt="" />
+                      </span>
+                      <p className="text-xl">{t("our_goals")}</p>
+                    </div>
+                    <div className=" flex justify-start items-center">
+                      <span className=" w-[40%] h-[100%] rounded-full bg-gradient-to-r from-[#997740] to-[#CCA972] flex justify-center items-center mr-2">
+                        <img src={chartBullish} className="w-[80%] h-[80%]" alt="" />
+                      </span>
+                      <p className="text-xl">{t("our_goals")}</p>
+                    </div>
+                    
+                    
+                    
+                    
+                  </div>
+                </div>
+              </div>
+            </div>
+            ) : (
+              <div className="w-full h-[100vh] grid grid-cols-1 gap-2 px-4">
+              <div className="h-[70vh] w-full relative flex justify-center items-center ">
+                <div className="absolute sm:w-[25%] sm:h-[55%] bg-[#E4C189] -z-[10] sm:top-[20px]  sm:right-[40px]"></div>
+                <div className="w-[80%] h-[80%] bg-white relative">
+                  <img
+                    src={Image1}
+                    className="object-cover w-full h-[80%]"
+                    alt=""
+                  />
+                </div>
+                <div className="absolute flex justify-center  md:rounded-md items-center w-[55%] h-[35%] bg-[#fff] z-[10]  bottom-[80px] left-0">
+                  <img
+                    src={Image1}
+                    className=" w-[92%] h-[88%] round"
+                    alt=""
+                  />
+                </div>
+              </div>
+              <div className="h-full w-full">
+                <div className=" pl-4 ">
+                  <div className="flex items-start justify-start py-16">
+                    <div className="w-3 h-10 rounded-md bg-[#CCA972] ml-2 bg-gradient-to-r from-[#A27942] "></div>
+                    <h1 className="text-3xl font-extrabold">
+                      {t("homePage2")}
+                    </h1>
+                  </div>
+                  <div className="text-start">
+                    <p className="text-justify  text-[#5B5B5B] leading-7">
+                      شركه عالم الاعمال هي شركه وطنية ذات مسؤولية محدودة.أسسها
+                      الدكتور ربيع بن علي العوبثاني في مدينة المكلا و حضرموت
+                      في مارس 2021م تحمل سجل تجاري رقم(21/2831).
+                    </p>
+                    <p className="text-justify  mt-8 text-[#5B5B5B] leading-8">
+                      تعمل على إدارة وتشغيل المشاريع الاستثمارية وتقديم
+                      الدراسات الاقتصادية والاستشارات الادارية والمالية
+                      والتسويقية وحلول تكنولوجيا المعلومات وتقدم خدماتها من
+                      خلال مجموعة من الخبراء والاستشاريين المتخصصين الذين
+                      يعملون في الشركة ومجموعة اخرى من الخبراء والاستشاريين
+                      المتعاقدين.
+                    </p>
+                  </div>
+                  <div className="grid grid-cols-3 gap-2 mt-6">
+                    <div className=" flex justify-start items-center">
+                      <span className=" w-[40%] h-[100%] rounded-full bg-gradient-to-r from-[#997740] to-[#CCA972] flex justify-center items-center ml-2">
+                        <img src={partnership} className="w-[100%] h-[80%]" alt="" />
+                      </span>
+                      <p className="text-xl">{t("our_mission")}</p>
+                    </div>
+                    <div className=" flex justify-start items-center">
+                      <span className=" w-[40%] h-[100%] rounded-full bg-gradient-to-r from-[#997740] to-[#CCA972] flex justify-center items-center ml-2 mr-2">
+                        <img src={golas} className="w-[80%] h-[80%]" alt="" />
+                      </span>
+                      <p className="text-xl">{t("our_goals")}</p>
+                    </div>
+                    <div className=" flex justify-start items-center">
+                      <span className=" w-[40%] h-[100%] rounded-full bg-gradient-to-r from-[#997740] to-[#CCA972] flex justify-center items-center ml-2">
+                        <img src={chartBullish} className="w-[80%] h-[80%]" alt="" />
+                      </span>
+                      <p className="text-xl">{t("our_goals")}</p>
+                    </div>
+                    
+                    
+                    
+                    
+                  </div>
+                </div>
+              </div>
+            </div>
+            )}
+          </>
         )}
         {isTabletScreen && (
           <>
             {dir === "ltr" ? (
               <div className="w-full h-[100vh] grid grid-cols-1 gap-2 px-4">
                 <div className="h-[70vh] w-full relative flex justify-center items-center ">
-                  <div className="absolute md:w-[25%] md:h-[55%] bg-[#E4C189] -z-[10] md:top-[25px]  md:left-[60px]"></div>
+                  <div className="absolute md:w-[25%] md:h-[55%] bg-[#E4C189] -z-[10] md:top-[25px]  md:left-[40px]"></div>
                   <div className="w-[80%] h-[80%] bg-white relative">
                     <img
                       src={Image1}
-                      className="object-cover w-full h-full"
+                      className="object-cover w-full h-[80%]"
                       alt=""
                     />
                   </div>
-                  <div className="absolute flex justify-center  md:rounded-md items-center w-[45%] h-[45%] bg-[#fff] z-[10]  bottom-[0px] right-0">
+                  <div className="absolute flex justify-center  md:rounded-md items-center w-[55%] h-[35%] bg-[#fff] z-[10]  bottom-[80px] right-0">
                     <img
                       src={Image1}
                       className=" w-[92%] h-[88%] round"
@@ -62,20 +199,64 @@ export default function AboutUsHeader() {
                     />
                   </div>
                 </div>
-                <div className="h-[90vh] w-full bg-black">a</div>
+                <div className="h-full w-full">
+                  <div className=" pl-4 ">
+                    <div className="flex items-start justify-start py-16">
+                      <div className="w-3 h-10 rounded-md bg-[#CCA972] mr-2 bg-gradient-to-r from-[#A27942] "></div>
+                      <h1 className="text-3xl font-extrabold">
+                        {t("homePage2")}
+                      </h1>
+                    </div>
+                    <div>
+                      <p className="text-start  text-[#5B5B5B] leading-7">
+                        شركه عالم الاعمال هي شركه وطنية ذات مسؤولية محدودة.أسسها
+                        الدكتور ربيع بن علي العوبثاني في مدينة المكلا و حضرموت
+                        في مارس 2021م تحمل سجل تجاري رقم(21/2831).
+                      </p>
+                      <p className="text-start  mt-8 text-[#5B5B5B] leading-8">
+                        تعمل على إدارة وتشغيل المشاريع الاستثمارية وتقديم
+                        الدراسات الاقتصادية والاستشارات الادارية والمالية
+                        والتسويقية وحلول تكنولوجيا المعلومات وتقدم خدماتها من
+                        خلال مجموعة من الخبراء والاستشاريين المتخصصين الذين
+                        يعملون في الشركة ومجموعة اخرى من الخبراء والاستشاريين
+                        المتعاقدين.
+                      </p>
+                    </div>
+                    <div className="grid grid-cols-3 mt-6">
+                      <div className="w-[95%] h-[100px] flex justify-center items-center">
+                        <span className=" w-[40%] h-[90%] rounded-full bg-gradient-to-r from-[#997740] to-[#CCA972] flex justify-center items-center mr-4">
+                          <img src={partnership} />
+                        </span>
+                        <p className="text-3xl">{t("our_mission")}</p>
+                      </div>
+                      <div className="w-[95%] h-[100px] flex justify-center items-center">
+                        <span className=" w-[40%] h-[90%] rounded-full bg-gradient-to-r from-[#997740] to-[#CCA972] flex justify-center items-center mr-4">
+                          <img src={golas} alt="" />
+                        </span>
+                        <p className="text-3xl">{t("our_goals")}</p>
+                      </div>
+                      <div className="w-[95%] h-[100px] flex justify-center items-center">
+                        <span className=" w-[40%] h-[90%] rounded-full bg-gradient-to-r from-[#997740] to-[#CCA972] flex justify-center items-center mr-4">
+                          <img src={chartBullish} alt="" />
+                        </span>
+                        <p className="text-3xl">{t("our_message")}</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
             ) : (
               <div className="w-full h-[100vh] grid grid-cols-1 gap-2 px-4">
                 <div className="h-[70vh] w-full relative flex justify-center items-center ">
-                  <div className="absolute md:w-[25%] md:h-[55%] bg-[#E4C189] -z-[10] md:top-[25px]  md:right-[60px]"></div>
+                  <div className="absolute md:w-[25%] md:h-[55%] bg-[#E4C189] -z-[10] md:top-[25px]  md:right-[40px]"></div>
                   <div className="w-[80%] h-[80%] bg-white relative">
                     <img
                       src={Image1}
-                      className="object-cover w-full h-full"
+                      className="object-cover w-[100%] h-[80%]"
                       alt=""
                     />
                   </div>
-                  <div className="absolute flex justify-center  md:rounded-md items-center w-[45%] h-[45%] bg-[#fff] z-[10]  bottom-[0px] left-0">
+                  <div className="absolute flex justify-center  md:rounded-md items-center w-[55%] h-[35%] bg-[#fff] z-[10]  bottom-[80px] left-0">
                     <img
                       src={Image1}
                       className=" w-[92%] h-[88%] round"
@@ -83,7 +264,51 @@ export default function AboutUsHeader() {
                     />
                   </div>
                 </div>
-                <div className="h-[90vh] w-full bg-black">a</div>
+                <div className="h-full w-full">
+                  <div className=" pl-4 ">
+                    <div className="flex items-start justify-start py-16">
+                      <div className="w-3 h-10 rounded-md bg-[#CCA972] ml-2 bg-gradient-to-r from-[#A27942] "></div>
+                      <h1 className="text-3xl font-extrabold">
+                        {t("homePage2")}
+                      </h1>
+                    </div>
+                    <div>
+                      <p className="text-start  text-[#5B5B5B] leading-7">
+                        شركه عالم الاعمال هي شركه وطنية ذات مسؤولية محدودة.أسسها
+                        الدكتور ربيع بن علي العوبثاني في مدينة المكلا و حضرموت
+                        في مارس 2021م تحمل سجل تجاري رقم(21/2831).
+                      </p>
+                      <p className="text-start  mt-8 text-[#5B5B5B] leading-8">
+                        تعمل على إدارة وتشغيل المشاريع الاستثمارية وتقديم
+                        الدراسات الاقتصادية والاستشارات الادارية والمالية
+                        والتسويقية وحلول تكنولوجيا المعلومات وتقدم خدماتها من
+                        خلال مجموعة من الخبراء والاستشاريين المتخصصين الذين
+                        يعملون في الشركة ومجموعة اخرى من الخبراء والاستشاريين
+                        المتعاقدين.
+                      </p>
+                    </div>
+                    <div className="grid grid-cols-3 mt-6">
+                      <div className="w-[95%] h-[100px] flex justify-center items-center">
+                        <span className=" w-[40%] h-[90%] rounded-full bg-gradient-to-r from-[#997740] to-[#CCA972] flex justify-center items-center ml-4">
+                          <img src={partnership} />
+                        </span>
+                        <p className="text-3xl">{t("our_mission")}</p>
+                      </div>
+                      <div className="w-[95%] h-[100px] flex justify-center items-center">
+                        <span className=" w-[40%] h-[90%] rounded-full bg-gradient-to-r from-[#997740] to-[#CCA972] flex justify-center items-center ml-4">
+                          <img src={golas} alt="" />
+                        </span>
+                        <p className="text-3xl">{t("our_goals")}</p>
+                      </div>
+                      <div className="w-[95%] h-[100px] flex justify-center items-center">
+                        <span className=" w-[40%] h-[90%] rounded-full bg-gradient-to-r from-[#997740] to-[#CCA972] flex justify-center items-center ml-4">
+                          <img src={chartBullish} alt="" />
+                        </span>
+                        <p className="text-3xl">{t("our_message")}</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
             )}
           </>
