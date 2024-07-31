4b68bcf1741d405d2eae79de05ea609586070afc
H-7117 <hamod2131.a@gmail.com>
2024-07-31 14:41:04 +0300

    done

--- src/components/(Admin)/sideNab.tsx
+++ src/components/(Admin)/sideNab.tsx
@@ -22,36 +22,34 @@ export default function SideNab() {
       {dir === "ltr" ? (
         <>
           <ul
-            className={`w-full mb-5 px-3 duration-500 ease-in-out ${
-              publishes ? "min-h-[40%]" : "h-[20%]"
-            } `}
+            className={`w-full mb-5 px-3 duration-500 ease-in-out ${"min-h-[30%]"} `}
           >
-            <div className="float-end w-[90%] h-[1px] bg-[#E5E5E5] mb-3"></div>
-            <p className="text-[12px] text-[#9E9E9E]">المنشورات</p>
-            <li className="mt-1 p-2 rounded-md flex justify-end items-center">
-              المراجع
-              <i className="ml-2">
+            <div className="float-start w-[90%] h-[1px] bg-[#E5E5E5] mb-3"></div>
+            <p className="text-[12px] text-start text-[#9E9E9E]">Publications</p>
+            <li className="mt-1 p-2 rounded-md flex justify-start items-center">
+              <i className="mr-2">
                 <Refernce />
               </i>
+              References
             </li>
-            <li className="mt-1 py-2 px-1 rounded-md flex justify-end items-center">
-              التقارير
-              <i className="ml-2">
+            <li className="mt-1 py-2 px-1 rounded-md flex justify-start items-center">
+              <i className="mr-2">
                 <Reports />
               </i>
+              Reports
             </li>
             <h1
               className="hover:bg-[rgb(213,174,120)]/[.20] rounded-md p-2  hover:text-[#D5AE78] cursor-pointer flex justify-between items-center"
               onClick={() => setPublishes((open) => !open)}
             >
-              <div>
-                {publishes === false ? <IoIosArrowUp /> : <IoIosArrowDown />}
-              </div>
               <div className="flex">
-                المنشورات
-                <i className="ml-2">
+                <i className="mr-2">
                   <PublichesIcon />
                 </i>
+                Publications
+              </div>
+              <div>
+                {publishes === false ? <IoIosArrowUp /> : <IoIosArrowDown />}
               </div>
             </h1>
             {publishes && (
@@ -60,84 +58,84 @@ export default function SideNab() {
                   publishes ? "max-height-[200px]" : "max-height-0"
                 }`}
               >
-                <li className="px-5 mt-1 p-2 rounded-md flex justify-end items-center">
-                  المنشورات
-                  <i className="ml-2">
+                <li className="px-5 mt-1 p-2 rounded-md flex justify-start items-center">
+                  <i className="mr-2">
                     <img src={NewsIcon} alt="" />
                   </i>
+                  Publications
                 </li>
-                <li className="px-5 mt-1 p-2 rounded-md flex justify-end items-center">
-                  الاخبار
-                  <i className="ml-2">
+                <li className="px-5 mt-1 p-2 rounded-md flex justify-start items-center">
+                  <i className="mr-2">
                     <img src={NewsIcon2} alt="" />
                   </i>
+                  News
                 </li>
-                <li className="px-5 mt-1 p-2 rounded-md flex justify-end items-center">
-                  التحاليل
-                  <i className="ml-2">
+                <li className="px-5 mt-1 p-2 rounded-md flex justify-start items-center">
+                  <i className="mr-2">
                     <StudiesIcon />
                   </i>
+                  Analysis
                 </li>
               </ul>
             )}
           </ul>
 
           <ul className="w-full min-h-[25%]  mb-5 px-3">
-            <div className="float-end w-[90%] h-[1px] bg-[#E5E5E5] mb-3"></div>
-            <p className="text-[12px] text-[#9E9E9E]">المؤسسات و الموزفين</p>
-            <li className="px-5 mt-1 p-2 flex justify-end items-center">
-              فريق العمل
-              <i className="ml-2">
+            <div className="float-start w-[90%] h-[1px] bg-[#E5E5E5] mb-3"></div>
+            <p className="text-[12px] text-start text-[#9E9E9E]">Organizations and Employees</p>
+            <li className="px-3 mt-1 p-2 flex justify-start items-center">
+              <i className="mr-2">
                 <img src={Teams} alt="" />
               </i>
+              Task Force
             </li>
-            <li className="px-5 mt-1 p-2 flex justify-end items-center">
-              المؤسسات التي نديرها
-              <i className="ml-2">
+            <li className="px-3 mt-1 p-2 flex justify-start items-center">
+              <i className="mr-2">
                 <Orgnaztion />
               </i>
+              Institutions We Manage
             </li>
-            <li className="px-5 mt-1 p-2 flex justify-end items-center">
-              شركائنا
-              <i className="ml-2">
+            <li className="px-3 mt-1 p-2 flex justify-start items-center">
+              <i className="mr-2">
                 <Massage />
               </i>
+              Our Partners
             </li>
-            <li className="px-5 mt-1 p-2 flex justify-end items-center">
-              الكُتَّاب
-              <i className="ml-2">
+            <li className="px-3 mt-1 p-2 flex justify-start items-center">
+              <i className="mr-2">
                 <Massage />
               </i>
+              Writers
             </li>
           </ul>
 
           <ul className="w-full min-h-[30%] px-3">
-            <div className="float-end w-[90%] h-[1px] bg-[#E5E5E5] mb-3"></div>
-            <p className="text-[12px] text-[#9E9E9E]">المزايا</p>
-            <li className="px-5 mt-1 p-2 rounded-md flex justify-end items-center">
-              الوظائف
-              <i className="ml-2">
+            <div className="float-start w-[90%] h-[1px] bg-[#E5E5E5] mb-3"></div>
+            <p className="text-[12px] text-start text-[#9E9E9E]">Features</p>
+            <li className="px-3 mt-1 p-2 rounded-md flex justify-start items-center">
+              <i className="mr-2">
                 <img src={Busniess} alt="" />
               </i>
+              Job
             </li>
-            <li className="px-5 mt-1 p-2 rounded-md flex justify-end items-center">
-              خدماتنا
-              <i className="ml-2">
+            <li className="px-3 mt-1 p-2 rounded-md flex justify-start items-center">
+              <i className="mr-2">
                 <img src={Service} alt="" />
               </i>
+              Our Services
             </li>
-            <li className="px-5 mt-1 p-2 flex justify-end items-center">
-              ملف تعريقي لاعمالنا
-              <i className="ml-2">
+            <li className="px-3 mt-1 p-2 flex justify-start items-center">
+              <i className="mr-2">
                 <OurFiles />
               </i>
+              Portfolio
             </li>
           </ul>
         </>
       ) : (
         <>
           <ul
-            className={`w-full mb-5 px-3 duration-500 ease-in-out ${"min-h-[40%]"} `}
+            className={`w-full mb-5 px-3 duration-500 ease-in-out ${"min-h-[30%]"} `}
           >
             <div className="float-start w-[90%] h-[1px] bg-[#E5E5E5] mb-3"></div>
             <p className="text-[12px] text-right text-[#9E9E9E]">المنشورات</p>
