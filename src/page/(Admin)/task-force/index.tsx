7ddd395d8e83426f399c81105859bb72398c250a
H-7117 <hamod2131.a@gmail.com>
2024-09-16 23:15:46 +0300

    complete add taskforce

--- src/page/(Admin)/task-force/index.tsx
+++ src/page/(Admin)/task-force/index.tsx
@@ -1,21 +1,36 @@
 import React from "react";
 import TaskForceTable from "src/components/table/taskforce-table";
 import Breadcrumb from "src/ui/breadcrumb";
+import { useTranslation } from "react-i18next";
+import EnBreadcrumb from "src/ui/en-breadcrumb";
 
 export default function TaskForceIndex() {
+  const { t, i18n } = useTranslation();
+  const dir = i18n.dir();
   return (
     <main>
       <div className="fixed h-screen  w-[83.5%] text-right bg-[#f2f2f2]">
         <div className="grid grid-cols-1">
           <div className="col-span-1 mb-2 mt-4 h-auto  rounded-lg">
-            <Breadcrumb
-              tilte1="المؤسسات و الموظفين"
-              path1="/attendance"
-              tilte2=" فريق العمل"
-              path2="/attendance/add-Attendance"
-              tilte3=""
-              path3=""
-            />
+            {dir === "ltr" ? (
+              <EnBreadcrumb
+                tilte1="Organizations and Employees"
+                path1="/attendance"
+                tilte2="Task Force"
+                path2="/attendance/add-Attendance"
+                tilte3=""
+                path3=""
+              />
+            ) : (
+              <Breadcrumb
+                tilte1="المؤسسات و الموظفين"
+                path1="/attendance"
+                tilte2=" فريق العمل"
+                path2="/attendance/add-Attendance"
+                tilte3=""
+                path3=""
+              />
+            )}
           </div>
         </div>
 
