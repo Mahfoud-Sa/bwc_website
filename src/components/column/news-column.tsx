2411f97066987d583f184d1dbfe66446aa38a451
H-7117 <hamod2131.a@gmail.com>
2024-10-06 13:09:38 +0300

    fixed the design of news page in admin

---
+++ src/components/column/news-column.tsx
@@ -0,0 +1,161 @@
+import { type ColumnDef } from "@tanstack/react-table";
+import { Eye } from "lucide-react";
+import { Button } from "../../ui/button";
+import DeleteDialog from "../dailog/delete-dialog";
+import { Link } from "react-router-dom";
+import EditIcon from "src/assets/icons/edit-icon";
+import Tooltip from "src/ui/tooltap";
+import ChangePublishesDialog from "../dailog/change-publish";
+
+export type AddNewOrder = {
+  isSelected: boolean;
+  id: number;
+  name: string;
+  img: string;
+  link: string;
+};
+
+export const AddNewsColumns: ColumnDef<AddNewOrder>[] = [
+  {
+    id: "img",
+    accessorKey: "img",
+    header: "ص",
+    cell: ({ row }) => {
+      return (
+        <div className=" w-[50px] h-[50px] rounded-full">
+          <img
+            src={row.original.img}
+            className="w-full h-full object-cover"
+            alt=""
+          />
+        </div>
+      );
+    },
+  },
+  {
+    accessorKey: "name",
+    header: "عنوان الخبر",
+  },
+  {
+    accessorKey: "link",
+    header: "تارخ الخبر",
+  },
+
+  {
+    accessorKey: "link",
+    header: "نشر/الغاء النشر",
+  },
+
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      //   const { data: session } = useSession();
+
+      return (
+        <div className="flex justify-center ">
+          <Link to={`/admin-dashboard/jobs/update-job/${row.original?.id}`}>
+            <Tooltip text="تعديل">
+              <Button
+                className="bg-[#d5ae78] text-white ml-3 rounded-lg"
+                size={"sm"}
+              >
+                <EditIcon />
+              </Button>
+            </Tooltip>
+          </Link>
+          <Link to={`/admin-dashboard/jobs/info/${row.original.id}`}>
+            <Tooltip text="عرض">
+              <Button
+                className="bg-[#d5ae78] text-white ml-3 rounded-lg"
+                size={"sm"}
+              >
+                <Eye className="" />
+              </Button>
+            </Tooltip>
+          </Link>
+          <Tooltip text="تغير حالة النشر">
+            <ChangePublishesDialog id={row.original.id} />
+          </Tooltip>
+          <Tooltip text="حذف">
+            <DeleteDialog
+              url={`/api/Jobs/${row.original?.id}`}
+              path={"/admin-dashboard/jobs"}
+            />
+          </Tooltip>
+        </div>
+      );
+    },
+  },
+];
+
+export const AddENNewsColumns: ColumnDef<AddNewOrder>[] = [
+  {
+    id: "img",
+    accessorKey: "img",
+    header: "img",
+    cell: ({ row }) => {
+      return (
+        <div className=" w-[50px] h-[50px] rounded-full">
+          <img
+            src={row.original.img}
+            className="w-full h-full object-cover"
+            alt=""
+          />
+        </div>
+      );
+    },
+  },
+  {
+    accessorKey: "name",
+    header: "news title",
+  },
+  {
+    accessorKey: "link",
+    header: "Date of news",
+  },
+  {
+    accessorKey: "link",
+    header: "publishing/unpublication",
+  },
+
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      //   const { data: session } = useSession();
+
+      return (
+        <div className="flex justify-center ">
+          <Link to={`/admin-dashboard/jobs/update-job/${row.original?.id}`}>
+            <Tooltip text="Edit">
+              <Button
+                className="bg-[#d5ae78] text-white ml-3 rounded-lg"
+                size={"sm"}
+              >
+                <EditIcon />
+              </Button>
+            </Tooltip>
+          </Link>
+          <Link to={`/admin-dashboard/jobs/info/${row.original.id}`}>
+            <Tooltip text="view">
+              <Button
+                className="bg-[#d5ae78] text-white ml-3 rounded-lg"
+                size={"sm"}
+              >
+                <Eye className="" />
+              </Button>
+            </Tooltip>
+          </Link>
+          <Tooltip text="Change publishing status">
+            <ChangePublishesDialog id={row.original.id} />
+          </Tooltip>
+          <Tooltip text="delete">
+            <DeleteDialog
+              url={`/api/Jobs/${row.original?.id}`}
+              path={"/admin-dashboard/jobs"}
+            />
+          </Tooltip>
+        </div>
+      );
+    },
+  },
+];
--- src/page/(Admin)/Publications/News/index.tsx
+++ src/page/(Admin)/Publications/News/index.tsx
@@ -1,20 +1,35 @@
+import { useTranslation } from "react-i18next";
 import NewsTable from "src/components/table/new-table";
 import Breadcrumb from "src/ui/breadcrumb";
+import EnBreadcrumb from "src/ui/en-breadcrumb";
 
 export default function NewsIndex() {
+  const { t, i18n } = useTranslation();
+  const dir = i18n.dir();
   return (
     <main>
       <div className="fixed h-screen  w-[83.5%] text-right bg-[#f2f2f2]">
         <div className="grid grid-cols-1">
           <div className="col-span-1 mb-2 mt-4 h-auto  rounded-lg">
-            <Breadcrumb
-              tilte1="المنشورات"
-              path1="/attendance"
-              tilte2=" منشورات"
-              path2="/attendance/add-Attendance"
-              tilte3="الاخبار"
-              path3=""
-            />
+            {dir === "ltr" ? (
+              <EnBreadcrumb
+                tilte1="Publications"
+                path1="/attendance"
+                tilte2=" publications"
+                path2="/attendance/add-Attendance"
+                tilte3="news"
+                path3=""
+              />
+            ) : (
+              <Breadcrumb
+                tilte1="المنشورات"
+                path1="/attendance"
+                tilte2=" منشورات"
+                path2="/attendance/add-Attendance"
+                tilte3="الاخبار"
+                path3=""
+              />
+            )}
           </div>
         </div>
 
